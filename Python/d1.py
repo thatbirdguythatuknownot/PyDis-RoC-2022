c2ef34d8bfdf41e4905c00ea3ede74cd680a3036
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-08-11 17:40:19 +0800

    Update d1.py

--- Python/d1.py
+++ Python/d1.py
@@ -1,2 +1,7 @@
+# Part 1
 s = ...
 print((_:=s.count)('(')-_(')'))
+# Part 2
+print([i-2*s[:i].count(')')for i in range(len(s))].index(-1))
+# Both parts
+print(*[(f:=1)+[f:=f+(1|-(x==')'))for x in input()].index(0),f-1][::-1]) # Most of the logic taken from <@!397120199897120769>
