a6cc7520ca5b55888ce205c6bd93b9abe03f7d82
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-08-12 09:42:54 +0800

    Update d2.py

--- Python/d2.py
+++ Python/d2.py
@@ -1,13 +1,13 @@
 s=... # input
 # Part 1
 c=0;p=str.split
-for x in p(s,'\n'):l,w,h=map(int,p(x,'x'));c+=2*sum(y:=(l*w,w*h,h*l))+min(y)
+for x in p(s):l,w,h=map(int,p(x,'x'));c+=2*sum(y:=(l*w,w*h,h*l))+min(y)
 print(c)
 # Part 2
 c=0;p=str.split
-for x in p(s,'\n'):l,w,h=map(int,p(x,'x'));c+=2*sum(sorted([l,w,h])[:2])+l*w*h
+for x in p(s):l,w,h=map(int,p(x,'x'));c+=2*sum(sorted([l,w,h])[:2])+l*w*h
 print(c)
 # Both parts
 c=d=0;p=str.split
-for x in p(s,'\n'):l,w,h=map(int,p(x,'x'));c+=2*sum(y:=(l*w,w*h,h*l))+min(y);d+=2*sum(sorted([l,w,h])[:2])+l*w*h
+for x in p(s):l,w,h=map(int,p(x,'x'));c+=2*sum(y:=(l*w,w*h,h*l))+min(y);d+=2*sum(sorted([l,w,h])[:2])+l*w*h
 print(c,d,sep='\n')
