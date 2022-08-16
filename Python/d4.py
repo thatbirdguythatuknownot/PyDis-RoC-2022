e1cda13614debaa5a61a6df6cb2339089b32d60b
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-08-16 08:15:58 +0800

    Update d4.py

--- Python/d4.py
+++ Python/d4.py
@@ -1,7 +1,7 @@
 s=... # input
 # Part 1
-import hashlib;x=0;print(next(x for _ in iter(int,1)if hashlib.md5(f"{s}{(x:=x+1)}".encode()).hexdigest().startswith('0'*5)))
+import hashlib;x=0;print(next(x for _ in iter(int,1)if'0'*5==hashlib.md5(f"{s}{(x:=x+1)}".encode()).hexdigest()))
 # Part 2
-import hashlib;x=0;print(next(x for _ in iter(int,1)if hashlib.md5(f"{s}{(x:=x+1)}".encode()).hexdigest().startswith('0'*6)))
+import hashlib;x=0;print(next(x for _ in iter(int,1)if'0'*6==hashlib.md5(f"{s}{(x:=x+1)}".encode()).hexdigest()))
 # Both parts
-import hashlib;f=lambda n,x=0:print(next(x for _ in iter(int,1)if hashlib.md5(f"{s}{(x:=x+1)}".encode()).hexdigest().startswith('0'*n)));f(5);f(6)
+import hashlib;f=lambda n,x=0:print(next(x for _ in iter(int,1)if'0'*n==hashlib.md5(f"{s}{(x:=x+1)}".encode()).hexdigest()));f(5);f(6)
